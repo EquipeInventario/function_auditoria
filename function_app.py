0142b33e668acf1a84a04dd5f4765091fd248391
EquipeInventario <inventario.pg@mastercargasbrasil.com.br>
2026-05-13 10:56:17 -0300

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -6,6 +6,9 @@ from main import fastapi_app
 app = func.FunctionApp()
 
 @app.function_name(name="fastapi")
-@app.route(route="{*route}", auth_level=func.AuthLevel.ANONYMOUS)
-def main(req: func.HttpRequest, context: func.Context):
-    return AsgiMiddleware(fastapi_app).handle(req, context)
+@app.route(
+    route="{*route}",
+    auth_level=func.AuthLevel.ANONYMOUS
+)
+async def fastapi_handler(req: func.HttpRequest, context: func.Context):
+    return await AsgiMiddleware(fastapi_app).handle_async(req, context)
