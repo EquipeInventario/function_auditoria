968e0506c55d43a9e8abd06e400f828075602314
EquipeInventario <inventario.pg@mastercargasbrasil.com.br>
2026-05-13 09:56:31 -0300

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -3,8 +3,9 @@ from azure.functions import AsgiMiddleware
 
 from main import fastapi_app
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.FunctionApp()
 
-@app.route(route="{*route}")
+@app.function_name(name="fastapi")
+@app.route(route="{*route}", auth_level=func.AuthLevel.ANONYMOUS)
 def main(req: func.HttpRequest, context: func.Context):
     return AsgiMiddleware(fastapi_app).handle(req, context)
