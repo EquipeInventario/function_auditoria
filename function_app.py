8db91d288ea378e2c37e34ba3faa5b9c28779f0b
EquipeInventario <inventario.pg@mastercargasbrasil.com.br>
2026-06-19 08:40:17 -0300

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -4,13 +4,14 @@ from azure.functions import AsgiMiddleware
 from main import fastapi_app
 
 app = func.FunctionApp()
+
 asgi_middleware = AsgiMiddleware(fastapi_app)
 
 @app.function_name(name="fastapi")
 @app.route(
     route="{*route}",
     auth_level=func.AuthLevel.ANONYMOUS,
-    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 )
 async def fastapi_handler(req: func.HttpRequest, context: func.Context):
     return await asgi_middleware.handle_async(req, context)
